0af7b34a28075a22ecfcc79146c00b23b6814a36
Eli Uriegas <eliuriegas@meta.com>
2025-10-06 11:33:04 -0700

    Update
    [ghstack-poisoned]

--- reference/gpu-dev-cli/gpu_dev_cli/__init__.py
+++
@@ -1,3 +0,0 @@
-"""GPU Developer CLI Package"""
-
-__version__ = "0.1.0"
--- reference/terraform-gpu-devservers/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-gpu-devservers"
-    key            = "runners/terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "tfstate-lock-gpu-devservers"
-  }
-}
-
